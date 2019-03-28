f04e372849e5e194851d3c8cd6e93b64b17984e4
Sylvain Abadie <sylvain.abadie@frontapp.com>
2019-03-28 13:20:56 -0700

    :wrench: Support focus programmatically on iOS 12.2

--- FA_WKWebViewExtensions/Classes/WKWebView+focus.m
+++ FA_WKWebViewExtensions/Classes/WKWebView+focus.m
@@ -14,9 +14,15 @@
 + (void)allowDisplayingKeyboardWithoutUserAction {
   Class class = NSClassFromString(@"WKContentView");
   NSOperatingSystemVersion iOS_11_3_0 = (NSOperatingSystemVersion){11, 3, 0};
-  // TODO: Fail to compile when compiling above iOS 11.3 so that we know that we need to check
+  NSOperatingSystemVersion iOS_12_2_0 = (NSOperatingSystemVersion){12, 2, 0};
+  char * methodSignature = "_startAssistingNode:userIsInteracting:blurPreviousNode:changingActivityState:userObject:";
+  
+  if ([[NSProcessInfo processInfo] isOperatingSystemAtLeastVersion: iOS_12_2_0]) {
+    methodSignature = "_elementDidFocus:userIsInteracting:blurPreviousNode:changingActivityState:userObject:";
+  }
+  
   if ([[NSProcessInfo processInfo] isOperatingSystemAtLeastVersion: iOS_11_3_0]) {
-    SEL selector = sel_getUid("_startAssistingNode:userIsInteracting:blurPreviousNode:changingActivityState:userObject:");
+    SEL selector = sel_getUid(methodSignature);
     Method method = class_getInstanceMethod(class, selector);
     IMP original = method_getImplementation(method);
     IMP overridee = imp_implementationWithBlock(^void(id me, void* arg0, BOOL arg1, BOOL arg2, BOOL arg3, id arg4) {
